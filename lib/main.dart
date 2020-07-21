5bbea927fcf4f3f855e4d45bf9d2baff31ad727c
Mike Sheldon <mike@mikeasoft.com>
2020-07-21 15:56:16 +0100

    Add license information

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2020 Mike Sheldon <mike@mikeasoft.com>
+ *
+ * This file is part Myntan.
+ *
+ * Myntan is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * Myntan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import 'dart:io';
 import 'dart:core';
 import 'dart:async';
